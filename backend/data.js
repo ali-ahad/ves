c1e1733a32bc8c76ca7f7e4a6dd8b9a92359a101
Ali Ahad <aliahad99@link.cuhk.edu.hk>
2020-11-09 17:14:27 +0800

    Created node server

---
+++ backend/data.js
@@ -0,0 +1,60 @@
+const data = {
+  products: [
+    {
+      _id: '1',
+      name: 'Artwork 1',
+      image: '/images/artwork1.png',
+      category: 'Classical',
+      price: 120,
+      artist: 'Yamashiro',
+      description: 'Only for boujee individuals',
+    },
+    {
+      _id: '2',
+      name: 'Artwork 2',
+      image: '/images/artwork2.png',
+      category: 'Classical',
+      price: 120,
+      artist: 'Yamashiro',
+      description: 'Only for boujee individuals',
+    },
+    {
+      _id: '3',
+      name: 'Artwork 3',
+      image: '/images/artwork3.png',
+      category: 'Classical',
+      price: 120,
+      artist: 'Yamashiro',
+      description: 'Only for boujee individuals',
+    },
+    {
+      _id: '4',
+      name: 'Artwork 4',
+      image: '/images/artwork4.png',
+      category: 'Classical',
+      price: 120,
+      artist: 'Yamashiro',
+      description: 'Only for boujee individuals',
+    },
+    {
+      _id: '5',
+      name: 'Artwork 5',
+      image: '/images/artwork5.png',
+      category: 'Classical',
+      price: 120,
+      artist: 'Yamashiro',
+      description: 'Only for boujee individuals',
+    },
+    {
+      _id: '6',
+      name: 'Artwork 6',
+      image: '/images/artwork6.png',
+      category: 'Classical',
+      price: 120,
+      artist: 'Yamashiro',
+      description: 'Only for boujee individuals',
+    },
+  ],
+};
+
+export default data;
